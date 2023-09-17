01306e78d88f1900365c0031c7e59ff82a46d6e7
Jake Nelson <jake.nelzon@gmail.com>
2023-09-17 15:54:32 -0700

    collision working

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,18 +1,16 @@
-extends Area2D
-signal hit
+extends CharacterBody2D
 
-
-@export var velocity = Vector2.ZERO
-@export var velocity_max = Vector2(1000.0,1000.0)
-@export var velocity_min = Vector2(-1000.0,-1000.0)
+var velocity_max = Vector2(1000.0,1000.0)
+var velocity_min = Vector2(-1000.0,-1000.0)
 @export var velocity_angle = 45.0
 @export var acceleration = 10.0
+@export var rotation_speed = 0.01
 var theta = velocity_angle / 180.0 * PI
 var x_comp = cos(theta)
 var y_comp = sin(theta)
 var vel_vec_left = Vector2(x_comp, -1.0 * y_comp)
 var vel_vec_right = Vector2(-1.0 * x_comp, -1.0 * y_comp)
-@export var gravity_strength = 4.0
+@export var gravity_strength = 20.0
 var vec_gravity = Vector2(0,1.0) * gravity_strength
 
 var screen_size
@@ -26,12 +24,18 @@ func _ready():
 func _process(delta):
 	if Input.is_action_pressed("move_left"):
 		velocity += vel_vec_left * acceleration
+		rotation += rotation_speed
 	if Input.is_action_pressed("move_right"):
 		velocity += vel_vec_right * acceleration
+		rotation -= rotation_speed
+
+func _physics_process(delta):
+	
 	velocity += vec_gravity
 	#velocity = velocity.clamp(velocity_min, velocity_max)
-	position += velocity * delta
-	
-	
+	var collision_info = move_and_collide(velocity * delta)
+	if (collision_info):
+		velocity = velocity.bounce(collision_info.get_normal())
+		velocity *= 0.5
 		
-	
+
