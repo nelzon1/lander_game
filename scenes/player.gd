4291a62b7330812e70e4b3be4012ab0b42b901c8
Jake Nelson <jake.nelzon@gmail.com>
2023-09-17 13:59:36 -0700

    Player controls working

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,12 +3,17 @@ signal hit
 
 
 @export var velocity = Vector2.ZERO
-@export var velocity_max = Vector2(1000,1000)
-@export var velocity_min = Vector2(-1000,-1000)
-@export var velocity_angle = 45
-@export var acceleration = 1000
-var vel_vec_left = Vector2(cos( velocity_angle / 180 * PI), 1 * sin( velocity_angle / 180 * PI))
-var vel_vec_right = Vector2(-1 * cos( velocity_angle / 180 * PI), 1 * sin( velocity_angle / 180 * PI))
+@export var velocity_max = Vector2(1000.0,1000.0)
+@export var velocity_min = Vector2(-1000.0,-1000.0)
+@export var velocity_angle = 45.0
+@export var acceleration = 10.0
+var theta = velocity_angle / 180.0 * PI
+var x_comp = cos(theta)
+var y_comp = sin(theta)
+var vel_vec_left = Vector2(x_comp, -1.0 * y_comp)
+var vel_vec_right = Vector2(-1.0 * x_comp, -1.0 * y_comp)
+@export var gravity_strength = 4.0
+var vec_gravity = Vector2(0,1.0) * gravity_strength
 
 var screen_size
 
@@ -19,15 +24,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_left"):
-		velocity.x = velocity.x + (cos( velocity_angle / 180 * PI) * acceleration)
-		velocity.y = velocity.y - (sin( velocity_angle / 180 * PI) * acceleration) * 100
+		velocity += vel_vec_left * acceleration
 	if Input.is_action_pressed("move_right"):
-		velocity.x = velocity.x - (cos( velocity_angle / 180 * PI) * acceleration)
-		velocity.y = velocity.y - (sin( velocity_angle / 180 * PI) * acceleration) * 100
-		
+		velocity += vel_vec_right * acceleration
+	velocity += vec_gravity
 	#velocity = velocity.clamp(velocity_min, velocity_max)
 	position += velocity * delta
 	
